b51c92dc5998b2ff956568d865cea4138227a002
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-03-04 02:30:08 +0300

    upd

--- ULBIandOTHERS.js
+++ ULBIandOTHERS.js
@@ -244,7 +244,26 @@ alert(map.size); // 3
 
 
 
+let recipeMap = new Map([
+    ["огурец", 500],
+    ["помидор", 350],
+    ["лук", 50]
+]);
 
+// перебор по ключам (овощи)
+for (let vegetable of recipeMap.keys()) {
+    alert(vegetable); // огурец, помидор, лук
+}
+
+// перебор по значениям (числа)
+for (let amount of recipeMap.values()) {
+    alert(amount); // 500, 350, 50
+}
+
+// перебор по элементам в формате [ключ, значение]
+for (let entry of recipeMap) { // то же самое, что и recipeMap.entries()
+    alert(entry); // огурец,500 (и так далее)
+}
 
 
 
